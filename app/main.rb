314b42f984d9ee77602e15698ab59186d6f1e941
Jason Etheridge <phasefx@gmail.com>
2020-06-21 01:01:45 -0400

    stub out serialize replacement

--- app/main.rb
+++ app/main.rb
@@ -8,6 +8,18 @@ class PhaseFX
     @args = args
   end
 
+  def serialize
+    {}
+  end
+
+  def inspect
+    serialize.to_s
+  end
+
+  def to_s
+    serialize.to_s
+  end
+
   #############################################################################
   # collect input
 
