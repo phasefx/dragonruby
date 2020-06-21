0a01185606d5cc2b6693d13b49a8ccea47381bcb
Jason Etheridge <phasefx@gmail.com>
2020-06-21 00:58:24 -0400

    init

---
+++ app/main.rb
@@ -0,0 +1,53 @@
+$gtk.reset
+
+class PhaseFX
+  #############################################################################
+  # setup
+  
+  def initialize args
+    @args = args
+  end
+
+  #############################################################################
+  # collect input
+
+  def input
+  end
+
+  #############################################################################
+  # handle the game logic
+
+  def logic
+  end
+
+  #############################################################################
+  # draw stuff
+ 
+  def render
+    render_something
+  end
+   
+  def render_something
+    @args.outputs.labels << [100, 100, "@", 255, 0, 0]
+  end
+
+
+  #############################################################################
+  # game loop
+
+  def tick
+    input
+    logic
+    render
+  end
+
+end
+
+###############################################################################
+# main
+
+def tick args
+  args.state.game ||= PhaseFX.new args
+  args.state.game.tick
+
+end
