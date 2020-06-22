cdeb971d240b9bdf72d505211e0f625785af038b
Jason Etheridge <phasefx@gmail.com>
2020-06-22 01:09:09 -0400

    progress

--- app/main.rb
+++ app/main.rb
@@ -6,12 +6,21 @@ class PhaseFX
   
   def initialize args
     @args = args
-    @args.state.rotation = 0
-    @args.state.rotated_on = 0
-    @args.state.player = { :intend_x => 0, :intend_y => 0}
-    @args.state.player.x = @args.grid.rect[2].idiv(2)
-    @args.state.player.y = @args.grid.rect[3].idiv(2)
-    @args.state.sprite_idx = 1
+    @args.state.player = {
+      :intend_x => 0,
+      :intend_y => 0,
+      :speed_x => 10,
+      :speed_y => 10,
+      :collision_x => false,
+      :collision_y => false,
+      :x => @args.grid.rect[2].half,
+      :y => @args.grid.rect[3].half,
+      :w => 128,
+      :h => 101,
+      :rotation => 0,
+      :rotated_on => 0,
+      :sprite_idx => 1
+    }
     @args.state.sprite_path = {
       :monsters => "sprites/DungeonAssetPack/SpriteFolder/Monsters/"
     }
@@ -21,9 +30,7 @@ class PhaseFX
 
   def serialize
     {
-      :rotation => @args.state.rotation,
-      :x => @args.state.x,
-      :y => @args.state.y
+      :player => @args.state.player
     }
   end # of serialize
 
@@ -56,29 +63,62 @@ class PhaseFX
 
     if @args.inputs.keyboard.key_held.right then rotate_right end
     if @args.inputs.keyboard.key_held.left then rotate_left end
-    if @args.inputs.keyboard.key_down.up
-        @args.state.sprite_idx += 1
-        puts "sprite_idx #{@args.state.sprite_idx}"
-    end
-    if @args.inputs.keyboard.key_down.down
-        @args.state.sprite_idx -= 1
-        puts "sprite_idx #{@args.state.sprite_idx}"
-    end
-    if @args.inputs.keyboard.key_down.pagedown
-        @args.state.sprite_idx = 1
-        puts "sprite_idx #{@args.state.sprite_idx}"
-    end
-    if @args.inputs.keyboard.key_down.pageup
-        @args.state.rotation = 0
-    end
+    if @args.inputs.keyboard.key_down.up then @args.state.player[:sprite_idx] += 1 end
+    if @args.inputs.keyboard.key_down.down then @args.state.player[:sprite_idx] -= 1 end
+    if @args.inputs.keyboard.key_down.pagedown then @args.state.player[:sprite_idx] = 1 end
+    if @args.inputs.keyboard.key_down.pageup then @args.state.player[:rotation] = 0 end
+    if @args.state.player[:sprite_idx] < 1 then @args.state.player[:sprite_idx] = 1 end
+    if @args.state.player[:sprite_idx] > 15 then @args.state.player[:sprite_idx] = 15 end
 
     ###########################################################################
     # WASD
 
-    if @args.inputs.keyboard.key_held.w then intend_move_up end
-    if @args.inputs.keyboard.key_held.s then intend_move_down end
-    if @args.inputs.keyboard.key_held.a then intend_move_left end
-    if @args.inputs.keyboard.key_held.d then intend_move_right end
+    key_shift = false
+    key_x_axis = false
+    key_y_axis = false
+    key_w = false
+    key_a = false
+    key_s = false
+    key_d = false
+    if @args.inputs.keyboard.key_down.truthy_keys.length > 0 then
+      @args.inputs.keyboard.key_down.truthy_keys.each do |truth|
+        if truth == :shift then key_shift = true ; @args.state.player[:keypress_on] = @args.state.tick_count ; end
+        if truth == :w then key_w = true ; key_y_axis = true ; @args.state.player[:keypress_on] = @args.state.tick_count ; end
+        if truth == :a then key_a = true ; key_x_axis = true ; @args.state.player[:keypress_on] = @args.state.tick_count ; end
+        if truth == :s then key_s = true ; key_y_axis = true ; @args.state.player[:keypress_on] = @args.state.tick_count ; end
+        if truth == :d then key_d = true ; key_x_axis = true ; @args.state.player[:keypress_on] = @args.state.tick_count ; end
+      end
+    end
+    if @args.inputs.keyboard.key_up.truthy_keys.length > 0 then
+      @args.inputs.keyboard.key_up.truthy_keys.each do |truth|
+        if truth == :shift then key_shift = false end
+        if truth == :w then key_w = false end
+        if truth == :a then key_a = false end
+        if truth == :s then key_s = false end
+        if truth == :d then key_d = false end
+      end
+    end
+
+    if key_w then
+      intend_move_up key_shift ? 2 : 1
+    elsif !key_y_axis && !@args.inputs.keyboard.key_held.w && !@args.inputs.keyboard.key_held.s && @args.state.tick_count > @args.state.player[:keypress_on] + 30 then
+      intend_move_up 0
+    end
+    if key_a then
+      intend_move_left key_shift ? 2 : 1
+    elsif !key_x_axis && !@args.inputs.keyboard.key_held.a && !@args.inputs.keyboard.key_held.d && @args.state.tick_count > @args.state.player[:keypress_on] + 30 then
+      intend_move_left 0
+    end
+    if key_s then
+      intend_move_down key_shift ? 2 : 1
+    elsif !key_y_axis && !@args.inputs.keyboard.key_held.w && !@args.inputs.keyboard.key_held.s && @args.state.tick_count > @args.state.player[:keypress_on] + 30 then
+      intend_move_down 0
+    end
+    if key_d then
+      intend_move_right key_shift ? 2 : 1
+    elsif !key_x_axis && !@args.inputs.keyboard.key_held.a && !@args.inputs.keyboard.key_held.d && @args.state.tick_count > @args.state.player[:keypress_on] + 30 then
+      intend_move_right 0
+    end
 
     ###########################################################################
     # misc
@@ -89,66 +129,92 @@ class PhaseFX
   #############################################################################
   # handle the game logic
 
-  def intend_move_left
-    @args.state.player[:intend_x] = -1;
+  def intend_move_left relative_speed
+    puts "intend_move_left #{relative_speed}" if relative_speed != 0
+    @args.state.player[:intend_x] = -relative_speed;
   end
 
-  def intend_move_right
-    @args.state.player[:intend_x] = 1;
+  def intend_move_right relative_speed
+    puts "intend_move_right #{relative_speed}" if relative_speed != 0
+    @args.state.player[:intend_x] = relative_speed;
   end
 
-  def intend_move_up
-    @args.state.player[:intend_y] = 1;
+  def intend_move_up relative_speed
+    @args.state.player[:intend_y] = relative_speed;
   end
 
-  def intend_move_down
-    @args.state.player[:intend_y] = -1;
+  def intend_move_down relative_speed
+    @args.state.player[:intend_y] = -relative_speed;
   end
 
-  def move_left
-    @args.state.player.x -= 10
-    puts "move_left #{@args.state.player.x}"
+  def player_collision?
+    @args.state.player[:proposed_rect] = [ @args.state.player.x, @args.state.player.y, @args.state.player.w, @args.state.player.h ];
+    #puts "proposed_rect = #{@args.state.player[:proposed_rect]}"
+    return false;
   end
 
-  def move_right
-    @args.state.player.x += 10
-    puts "move_right #{@args.state.player.x}"
+  def move_left relative_speed
+    #puts "move_left #{relative_speed}"
+    @args.state.player.x -= @args.state.player[:speed_x] * relative_speed.abs
+    if @args.state.player.x < @args.grid.rect[0] - @args.state.player.w.half then
+      @args.state.player.x = @args.grid.rect[2] + @args.state.player.w.half
+    end
+    @args.state.player[:collision_x] = player_collision?
   end
 
-  def move_up
-    @args.state.player.y += 10
+  def move_right relative_speed
+    #puts "move_right #{relative_speed}"
+    @args.state.player.x += @args.state.player[:speed_x] * relative_speed.abs
+    if @args.state.player.x > @args.grid.rect[2] + @args.state.player.w.half then
+      @args.state.player.x = @args.grid.rect[0] - @args.state.player.w.half
+    end
+    @args.state.player[:collision_x] = player_collision?
   end
 
-  def move_down
-    @args.state.player.y -= 10
+  def move_up relative_speed
+    @args.state.player.y += @args.state.player[:speed_y] * relative_speed.abs
+    @args.state.player[:collision_y] = player_collision?
+  end
+
+  def move_down relative_speed
+    @args.state.player.y -= @args.state.player[:speed_y] * relative_speed.abs
+    @args.state.player[:collision_y] = player_collision? || @args.state.player.y < 0;
   end
 
   def rotate_left
-    if @args.state.rotation < 45 then @args.state.rotation += 1 else @args.state.rotation += 10 end
-    if @args.state.rotation > 270 then @args.state.rotation -= 360 end
-    @args.state.rotated_on = @args.state.tick_count
+    if @args.state.player[:rotation] < 45 then @args.state.player[:rotation] += 2 else @args.state.player[:rotation] += 10 end
+    if @args.state.player[:rotation] > 270 then @args.state.player[:rotation] -= 360 end
+    @args.state.player[:rotated_on] = @args.state.tick_count
   end
 
   def rotate_right
-    if @args.state.rotation > -45 then @args.state.rotation -= 1 else @args.state.rotation -= 10 end
-    if @args.state.rotation < -270 then @args.state.rotation += 360 end
-    @args.state.rotated_on = @args.state.tick_count
+    if @args.state.player[:rotation] > -45 then @args.state.player[:rotation] -= 2 else @args.state.player[:rotation] -= 10 end
+    if @args.state.player[:rotation] < -270 then @args.state.player[:rotation] += 360 end
+    @args.state.player[:rotated_on] = @args.state.tick_count
   end
 
   def intents
-    if @args.state.player[:intend_x] > 0 then move_right end
-    if @args.state.player[:intend_x] < 0 then move_left end
-    if @args.state.player[:intend_y] > 0 then move_up end
-    if @args.state.player[:intend_y] < 0 then move_down end
-    @args.state.player[:intend_x] = 0
-    @args.state.player[:intend_y] = 0
+    saved_x = @args.state.player.x
+    saved_y = @args.state.player.y
+    if @args.state.player[:intend_x] > 0 then move_right @args.state.player[:intend_x] end
+    if @args.state.player[:intend_x] < 0 then move_left @args.state.player[:intend_x] end
+    if @args.state.player[:intend_y] > 0 then move_up @args.state.player[:intend_y] end
+    if @args.state.player[:intend_y] < 0 then move_down @args.state.player[:intend_y] end
+    #@args.state.player[:intend_x] = 0
+    #@args.state.player[:intend_y] = 0
+    if @args.state.player[:collision_x] then
+      @args.state.player.x = saved_x
+    end
+    if @args.state.player[:collision_y] then
+      @args.state.player.y = saved_y
+    end
   end
 
   def iterate
 
     # slowly level out any rotation
-    if @args.state.rotation > 0 and @args.state.tick_count > @args.state.rotated_on + 10 then @args.state.rotation -= 0.5 end
-    if @args.state.rotation < 0 and @args.state.tick_count > @args.state.rotated_on + 10 then @args.state.rotation += 0.5 end
+    if @args.state.player[:rotation] > 0 and @args.state.tick_count > @args.state.player[:rotated_on] + 10 then @args.state.player[:rotation] -= 0.5 end
+    if @args.state.player[:rotation] < 0 and @args.state.tick_count > @args.state.player[:rotated_on] + 10 then @args.state.player[:rotation] += 0.5 end
 
     # handle intended movement
     intents
@@ -188,7 +254,7 @@ class PhaseFX
   end # of render_background
 
   def render_something
-    path = "#{@args.state.sprite_path[:monsters]}monster#{@args.state.sprite_idx}.png"
+    path = "#{@args.state.sprite_path[:monsters]}monster#{@args.state.player[:sprite_idx]}.png"
     face_left = false
     if @args.inputs.keyboard.directional_vector then
       face_left = @args.inputs.keyboard.directional_vector[0] < 0
@@ -201,7 +267,7 @@ class PhaseFX
       w: 128,
       h: 101,
       path: path,
-      angle: @args.state.rotation,
+      angle: @args.state.player[:rotation],
       a: 255,
       r: 255,
       g: 255,
@@ -234,4 +300,5 @@ end # of class PhaseFX
 def tick args
   args.state.game ||= PhaseFX.new args
   args.state.game.tick
+  #puts "60 ticks..." if args.state.tick_count % 60 == 0
 end # of tick
