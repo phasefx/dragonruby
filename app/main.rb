f6f38b45c903108e84d5fe1289b026ac8be9ce95
Jason Etheridge <phasefx@gmail.com>
2020-06-27 10:36:18 -0400

    hunting a $gtk.reset bug
    Signed-off-by: Jason Etheridge <phasefx@gmail.com>

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,3 @@
-$gtk.reset
 $gtk.set_window_title "PhaseFX"
 
 require 'app/Physics.rb'
@@ -22,5 +21,5 @@ def tick args
   args.state.game ||= Game.new args
   args.state.game.tick
   #puts "60 ticks..." if args.state.tick_count % 60 == 0
-  $gtk.reset seed: rand(Time.now.sec) if args.state[:reset_desired?]
+  $gtk.reset if args.state[:reset_desired?]
 end # of tick
